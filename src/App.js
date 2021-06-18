201864dd7c117637d9214a4cfb6166632b30db6b
Asad Arshad <asad.arshad@paysyslabs.com>
2021-06-18 18:48:51 +0500

    React Leaf Let Map

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React, { useEffect, useState } from "react";
 //import logo from './logo.svg';
-import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+
+// import "leaflet/dist/leaflet.css"
+
+import "./App.css"
 
 function App() {
-  const [repos, setRepos] = useState([{}]);
-
-  useEffect(() => {
-    async function FetchAPI() {
-      const api = await fetch("https://api.github.com/users/asad149/repos");
-
-      const json = await api.json();
-      console.log(json);
-      setRepos(json);
-    }
-    FetchAPI();
-  }, []);
 
   return (
     <div className="App">
-      <ul>
-        {repos.map((repoObj, ind) => {
-          return (
-            <li key={ind}>
-              <h2>{repoObj.name}</h2>
-            </li>
-          );
-        })}
-      </ul>
-
       <h1>My GitHub Repositries..!</h1>
+        <MapContainer center={[30.3753, 69.3451]} zoom={5} scrollWheelZoom={true} style={{height: "300"}}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[24.8607, 67.0011]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+
     </div>
   );
 }
